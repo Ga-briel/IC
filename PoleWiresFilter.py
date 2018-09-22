b3fa13d5887ceeef787a0b4835a137662a9fd86a
Gabriel Araújo <gabrielaraujo@Gabriels-MacBook-Air.local>
2018-09-22 19:54:51 -0300

    novo treinamento -> agora com validation

--- PoleWiresFilter.py
+++ PoleWiresFilter.py
@@ -1,64 +1,44 @@
 #imports from Keras package
-from keras.models import Sequential
+from keras.models import model_from_json
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-from keras.models import model_from_json
 ##
-import numpy as np
-#Based on Keras deep learning package
-def processImage():
-    classifier = Sequential()
-    classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
-    classifier.add(MaxPooling2D(pool_size = (2, 2)))
-    classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-    classifier.add(MaxPooling2D(pool_size = (2, 2)))
-    classifier.add(Flatten())
-    classifier.add(Dense(units = 128, activation = 'relu'))
-    classifier.add(Dense(units = 1, activation = 'sigmoid'))
-    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+from django_website.Primitives.GeoImage import GeoImage
+from matplotlib import pyplot as plt
 
-    from keras.preprocessing.image import ImageDataGenerator
+class PoleWiresFilter(ImageFilter):
+    #Based on Keras deep learning package
+    filterName = "PoleWires"
+    filterId = "PoleWires"
 
-    train_datagen = ImageDataGenerator(rescale = 1./255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True)
-    test_datagen = ImageDataGenerator(rescale = 1./255)
-    training_set = train_datagen.flow_from_directory('training_set',
-    target_size = (64, 64),
-    batch_size = 32,
-    class_mode = 'binary')
-    test_set = test_datagen.flow_from_directory('test_set',
-    target_size = (64, 64),
-    batch_size = 32,
-    class_mode = 'binary')
+    def _initialize(cls):
+        pass
+    def processImage(geoImage: GeoImage):
 
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("model.h5")
 
-    classifier.fit_generator(training_set,
-    steps_per_epoch = 1263,
-    epochs = 25,
-    validation_data = test_set,
-    validation_steps = 323)
-
-    #test set
-    from keras.preprocessing import image
-    test_image = image.load_img('test_set/comFio/144 67.png', target_size = (64, 64))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis = 0)
-    result = classifier.predict(test_image)
-    training_set.class_indices
-    if result[0][0] == 1:
-        prediction = 'comFio'
-    else:
-        prediction = 'semFio'
-
-    #saving classifier
-    model_json = classifier.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    classifier.save_weights("model.h5")
-    print("Model saved")
-
-processImage()
+        #test
+        from keras.preprocessing import image
+        #test_image = geoImage.data
+        from PIL import Image
+        #transformaçao da matriz em imagem
+        entry = geoImage.data
+        img = Image.fromarray(entry)
+        
+        test_image = image.load_img(img, target_size = (64,64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = loaded_model.predict(test_image)
+        training_set.class_indices
+        if result[0][0] == 1:
+            prediction = np.ones(640, 640)
+        else:
+            prediction = np.ones(640, 640)
+        return prediction
